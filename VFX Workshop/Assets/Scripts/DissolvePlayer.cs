90e650210140d9df41dc4ddc47d209f535489129
dj220679 <dj220679@FALMOUTH.AC.UK>
2019-12-10 11:26:38 +0000

    Script to change when dissolve shader dissolves. Particles to make it look cooler.
    Signed-off-by: dj220679 <dj220679@FALMOUTH.AC.UK>

---
+++ VFX Workshop/Assets/Scripts/DissolvePlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolvePlayer : MonoBehaviour
+{
+	public Transform EndPoint;
+	public ParticleSystem particles;
+	public Transform player;
+	public Material mat;
+
+	public Posseccing poss;
+
+	public float dissolveSpeed = 0.5f;
+
+	int dissolveDirection = 0;
+	float time = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+		var main = particles.main;
+		
+  
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			particles.transform.localRotation = new Quaternion();
+			particles.transform.localPosition = Vector3.zero;
+			main.startSpeed = Vector3.Distance(player.position, EndPoint.position);
+			
+			particles.Play();
+			dissolveDirection = 1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			particles.transform.position = EndPoint.position;
+			particles.transform.LookAt(player.position);
+			main.startSpeed = Vector3.Distance(EndPoint.position, player.position);
+
+			particles.Play();
+			dissolveDirection = -1;
+		}
+
+		if (dissolveDirection == 1)
+		{
+			time -= Time.deltaTime * dissolveSpeed;
+			mat.SetFloat("_DissolveHieght", time);
+			poss.glow = true;
+		}
+		if (dissolveDirection == -1)
+		{
+			time += Time.deltaTime * dissolveSpeed;
+			mat.SetFloat("_DissolveHieght", time);
+			poss.glow = false;
+		}
+
+    }
+}
---
+++ VFX Workshop/Assets/Scripts/Posseccing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Posseccing : MonoBehaviour
+{
+
+	public bool glow;
+	public Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		mat.SetColor("_ShineColor", new Color(0, 0, 0));
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (glow)
+		{
+			mat.SetColor("_ShineColor", new Color(0, 81, 191));
+		}
+		if (!glow)
+		{
+
+			mat.SetColor("_ShineColor", new Color(0, 0, 0));
+		}
+    }
+}
